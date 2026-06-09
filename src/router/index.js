3187e3404f5fbe12a449c97e716446e1383820e6
Micaela Galceran <galceranmica@gmail.com>
2026-06-09 20:37:02 -0300

    Listado de denuncias en proceso

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import LoginView from '../views/LoginView.vue'
+import LoginView from "../views/LoginView.vue";
+import ListadoDenunciasView from "@/views/ListadoDenunciasView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: LoginView
-    }
-  ]
-})
+      path: "/",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/denuncias",
+      name: "denuncias",
+      component: ListadoDenunciasView,
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/ListadoDenunciasView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="denuncias-container">
+    <h1>Mis Denuncias</h1>
+
+    <DenunciaCard
+      v-for="denuncia in denuncias"
+      :key="denuncia.id"
+      :denuncia="denuncia"
+    />
+  </div>
+</template>
+
+<script setup>
+import DenunciaCard from "@/components/denuncias/DenunciaCard.vue";
+
+const denuncias = [
+  {
+    id: 1,
+    titulo: "Robo de bicicleta",
+    descripcion: "Me robaron la bicicleta en la vía pública.",
+    estado: "En revisión",
+    fecha: "05/06/2026",
+  },
+  {
+    id: 2,
+    titulo: "Estafa online",
+    descripcion: "Realicé una compra en un sitio fraudulento.",
+    estado: "Aceptada",
+    fecha: "01/06/2026",
+  },
+];
+</script>
